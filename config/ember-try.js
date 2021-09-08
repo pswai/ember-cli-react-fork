e6370cfc84fbb598140edca0f63b492a764c7f51
pswai <parksoon_my@hotmail.com>
2021-09-09 06:48:58 +0800

    Update ember-try scenarios

--- config/ember-try.js
+++ config/ember-try.js
@@ -12,6 +12,47 @@ module.exports = function() {
       useYarn: true,
       scenarios: [
         {
+          name: 'ember-1.13',
+          bower: {
+            dependencies: {
+              ember: '~1.13.0',
+            },
+          },
+        },
+        {
+          name: 'ember-lts-2.4',
+          bower: {
+            dependencies: {
+              ember: '~2.4.0',
+            },
+          },
+        },
+        {
+          name: 'ember-lts-2.8',
+          bower: {
+            dependencies: {
+              ember: 'components/ember#lts-2-8',
+            },
+            resolutions: {
+              ember: 'lts-2-8',
+            },
+          },
+          npm: {
+            devDependencies: {
+              'ember-source': null,
+            },
+          },
+        },
+        // Glimmer was introduced in v2.10, so better test it
+        {
+          name: 'ember-2.10',
+          bower: {
+            dependencies: {
+              ember: '~2.10.0',
+            },
+          },
+        },
+        {
           name: 'ember-lts-2.12',
           npm: {
             devDependencies: {
@@ -32,6 +73,99 @@ module.exports = function() {
           npm: {
             devDependencies: {
               'ember-source': '~2.18.0',
+            },
+          },
+        },
+        {
+          name: 'ember-3.0',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.0.0',
+            },
+          },
+        },
+        {
+          name: 'ember-3.1',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.1.0',
+            },
+          },
+        },
+        // Ember internals changed
+        {
+          name: 'ember-3.2',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.2.0',
+            },
+          },
+        },
+        {
+          name: 'ember-lts-3.4',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.4.0',
+            },
+          },
+        },
+        // Ember internals changed
+        {
+          name: 'ember-3.6',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.6.0',
+            },
+          },
+        },
+        {
+          name: 'ember-lts-3.8',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.8.0',
+            },
+          },
+        },
+        {
+          name: 'ember-lts-3.12',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.12.0',
+            },
+          },
+        },
+        // First Octane preview
+        {
+          name: 'ember-3.13',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.13.0',
+            },
+          },
+        },
+        // Second Octane preview
+        {
+          name: 'ember-3.14',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.14.0',
+            },
+          },
+        },
+        // Octane release
+        {
+          name: 'ember-3.15',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.15.0',
+            },
+          },
+        },
+        {
+          name: 'ember-lts-3.16',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.16.0',
             },
           },
         },
